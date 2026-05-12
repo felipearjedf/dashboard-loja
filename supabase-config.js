e6bf0791c1a3ebc06a89ff8fac88817f12f19fa3
felipearjedf <felipearjedf@gmail.com>
2026-05-12 12:18:41 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,5 @@
-const supabaseUrl = 'https://myzkzqlynzkyvnhzfjzs.supabase.co';
-const supabaseKey = 'sb_publishable_S-9XdSdh9hjQSqxPiBBBLg_8x63JpHF';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+window.SUPABASE_CONFIG = {
+  url: "https://myzkzqlynzkyvnhzfjzs.supabase.co",
+  anonKey: "sb_publishable_S-9XdSdh9hjQSqxPiBBBLg_8x63JpHF",
+  recordId: "loja-principal"
+};
